eac380bf540ff46050a051c890a105a936b6dfa3
sumanta mandal <sumanta.mandal@jarvis.consulting>
2023-04-17 15:58:23 +0530

    added sidkiq to gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
+require 'sidekiq'
+require 'sidekiq/web'
 Rails.application.routes.draw do
   resources :posts
   mount SsoClient::Engine => "/sso_client", :as => "sso_client"
+  mount Sidekiq::Web => '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
